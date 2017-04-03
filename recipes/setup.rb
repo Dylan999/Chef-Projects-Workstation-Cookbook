ea3884f6988824b16dc333429faf4e80cb7ec08a
Cloud User <centos@ip-172-31-45-118.eu-west-1.compute.internal>
2017-04-03 14:08:27 +0000

    This is the inital workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,22 @@
+package 'tree' do
+ action :install
+end
+
+package 'git' do
+ action :install
+end
+
+package 'ntp' do 
+ action :install
+end
+
+file '/etc/motd' do
+ content 'This server belongs to Dylan, go away'
+ action :create
+ owner 'root'
+ group 'root'
+end
+
+service 'ntpd' do
+ action [ :enable, :start ]
+end
